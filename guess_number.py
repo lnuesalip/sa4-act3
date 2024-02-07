1a08dc1fcc8d612721a127ca5cb26796fec28e70
Learra Alip <lnuesalip@gmail.com>
2024-02-07 13:13:35 -0600

    Added a loop to allow the user to continuously guess, outputs try again message on wrong guess

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,15 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
-
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+guess = True
+while guess:
+    guess = input("What number am I thinking of? ")
+    
+    if guess == 'q':
+        print(f"Sorry! The number was {number}.")
+        break
+    elif int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        print(f"That's not correct. Try again.")
